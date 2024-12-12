e45becd38312fe031af1eeaadcdc4cb0d2ab4e27
Navrose <navrosesinghjohal@gmail.com>
2024-12-12 10:44:55 -0500

    Project initialized

---
+++ booting-web/booting-web/src/test/java/first/project/learning/booting_web/BootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package first.project.learning.booting_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
